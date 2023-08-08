49f953301ac9254cf53debaa66e51aeaa90c407d
Kevin Costa Oliveira <kevin_costa47@hotmail.com>
2023-08-08 18:01:44 -0300

    Carrosel-Finalizado

--- Views/header.php
+++ Views/header.php
@@ -3,15 +3,15 @@
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">
-    <script src="Js/Script.js"></script>
+    <link rel="stylesheet" href="../sitecompleto/Css/style.css">
+    <script src="../sitecompleto/Js/Script.js"></script>
     
     <title>Revista Gearhead</title>
 </head>
 <body>
 
     <header>
-    <nav>
-        
-    </nav>
+         <!-- <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            </div> -->
+        </nav>
     </header>
--- Views/home.php
+++ Views/home.php
@@ -1,5 +1,35 @@
-<div id="home">
+<main>
+    
     <div class="container">
-
-    </div>
-</div>
+            <div id="Carrosel" class="carousel slide" data-bs-ride="true">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#Carrosel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#Carrosel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#Carrosel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner" style="height: 30em;">
+                    <div class="carousel-item h-100 active" style="background-image:url(Img/Civic_TypeR_1920x700.jpg); background-size:cover; background-repeat: no-repeat; background-position: center">
+                        
+                    </div>
+                    <div class="carousel-item h-100" style="background:url(Img/audi2.jpg) center/cover no-repeat;">
+                       
+                    </div>
+                    <div class="carousel-item h-100" style="background:url(Img/haidong-liang-5EnoMxKb2W4-unsplash2.jpg) center/cover no-repeat;">
+                        
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#Carrosel" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#Carrosel" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        
+        <div class=row>
+            <p>................</p>
+        </div>
+    </div>  
+</main>
